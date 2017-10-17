27c3a6e8ca192e218c4901e456d42075cd59aa88
joeandrewgomez <joeandrewgomez@gmail.com>
2017-10-17 11:07:15 -0400

    this is a message

---
+++ src/com/joegomez/Main.java
@@ -0,0 +1,8 @@
+package com.joegomez;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("This is the beginning of the ArrayListCoursePractice project");
+    }
+}
